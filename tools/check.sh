fb75c922a470a4a9041ecb636fb46f8dff434724
Rootmhz <root28root@gmail.com>
2025-09-18 06:02:14 +0200

    Update check.sh

--- tools/check.sh
+++ tools/check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-projects=("mytoken" "escrow" "simplenft" "minimarket" "paysplit")
+projects=("mytoken" "escrow" "simplenft" "minimarket" "paysplit" "vesting")
 for p in "${projects[@]}"; do
   echo "===> '$p'"
   (cd "$p" && aptos move clean && aptos move compile && aptos move test --filter .)
